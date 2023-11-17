caa45247ffc052a734cf91ad687a2f30057e6c77
zerchen <zeruicasia@gmail.com>
2023-11-17 22:29:24 +0100

    fix sugar_box

--- vis_traj.py
+++ vis_traj.py
@@ -109,6 +109,20 @@ def main(traj_name='ycb-002_master_chef_can-20200709-subject-01-20200709_141754'
 
     env = TableEnv()
     object_model = object_generator(f"objects/{object_category}/{object_name}.xml")(pos=init_object_translation, quat=init_object_orientation)
+
+    object_model.mjcf_model.worldbody.add('body', name=f'hand_palm', pos=pregrasp_joint[0])
+    object_model.mjcf_model.worldbody.body[f'hand_palm'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_palm_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+    object_model.mjcf_model.worldbody.add('body', name=f'hand_thumb', pos=pregrasp_joint[4])
+    object_model.mjcf_model.worldbody.body[f'hand_thumb'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_thumb_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+    object_model.mjcf_model.worldbody.add('body', name=f'hand_index', pos=pregrasp_joint[8])
+    object_model.mjcf_model.worldbody.body[f'hand_index'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_index_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+    object_model.mjcf_model.worldbody.add('body', name=f'hand_middle', pos=pregrasp_joint[12])
+    object_model.mjcf_model.worldbody.body[f'hand_middle'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_middle_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+    object_model.mjcf_model.worldbody.add('body', name=f'hand_ring', pos=pregrasp_joint[16])
+    object_model.mjcf_model.worldbody.body[f'hand_ring'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_ring_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+    object_model.mjcf_model.worldbody.add('body', name=f'hand_little', pos=pregrasp_joint[20])
+    object_model.mjcf_model.worldbody.body[f'hand_little'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_little_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+
     for idx, _ in enumerate(traj_file['object_translation'][1:]):
         if idx % 1 == 0:
             object_model.mjcf_model.worldbody.add('body', name=f'object_marker_{idx}', pos=object_translation[idx], quat=object_orientation[idx])
