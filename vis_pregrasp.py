faadac1a0adee097638b003011f9c9f7eb08cf4d
zerchen <zeruicasia@gmail.com>
2023-11-30 14:48:57 +0100

    vis pregrasp

--- vis_pregrasp.py
+++ vis_pregrasp.py
@@ -7,9 +7,24 @@ from robots import get_robot
 from fire import Fire
 from base import MjModel
 import numpy as np
+import pickle
 import mujoco_viewer
 import mujoco
 import os
+
+
+def check_contacts(physics, geoms_1, geoms_2):
+    for contact in physics.data.contact[:physics.data.ncon]:
+        # check contact geom in geoms
+        c1_in_g1 = physics.model.geom(contact.geom1).name in geoms_1
+        c2_in_g2 = physics.model.geom(contact.geom2).name in geoms_2
+
+        # check contact geom in geoms (flipped)
+        c2_in_g1 = physics.model.geom(contact.geom2).name in geoms_1
+        c1_in_g2 = physics.model.geom(contact.geom1).name in geoms_2
+        if (c1_in_g1 and c2_in_g2) or (c1_in_g2 and c2_in_g1):
+            return True
+    return False
 
 
 class BaseEnv(MjModel):
@@ -55,6 +70,12 @@ def object_generator(path):
 
 
 def main(traj_name='ycb-002_master_chef_can-20200709-subject-01-20200709_141754'):
+    with open('trajectories/retarget_result_dexycb.pkl', 'rb') as f:
+        traj_robots = pickle.load(f)
+
+    index = '-'.join(traj_name.split('-')[2:-1]) + '/' + traj_name.split('-')[-1]
+    retarget_pose = np.array(traj_robots[index]['robot_joints'])
+
     object_category = traj_name.split('-')[0]
     object_name = traj_name.split('-')[1]
 
@@ -93,10 +114,18 @@ def main(traj_name='ycb-002_master_chef_can-20200709-subject-01-20200709_141754'
     # init_object_translation[:2] += new_init_pos
 
     env = TableEnv()
+
     robot_model = get_robot('adroit')(limp=False)
+    robot_mesh_list = robot_model.mjcf_model.find_all('geom')
+    robot_geom_names = [geom.get_attributes()['name'] for geom in robot_mesh_list]
+    robot_geom_names = [f'adroit/{name}' for name in robot_geom_names if 'C' in name]
     env.attach(robot_model)
 
     object_model = object_generator(f"objects/{object_category}/{object_name}.xml")(pos=init_object_translation, quat=init_object_orientation)
+    object_mesh_list = object_model.mjcf_model.find_all('geom')
+    object_geom_names = [geom.get_attributes()['name'] for geom in object_mesh_list]
+    object_geom_names = [f'{object_name}/{name}' for name in object_geom_names if 'contact' in name]
+
     object_model.mjcf_model.worldbody.add('body', name=f'hand_palm', pos=pregrasp_joint[0])
     object_model.mjcf_model.worldbody.body[f'hand_palm'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_palm_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
     object_model.mjcf_model.worldbody.add('body', name=f'hand_thumb', pos=pregrasp_joint[4])
@@ -112,21 +141,31 @@ def main(traj_name='ycb-002_master_chef_can-20200709-subject-01-20200709_141754'
 
     env.attach(object_model)
     physics = physics_from_mjcf(env)
+    ori_obs_pos = physics.named.data.xpos[45].copy()
 
     model = physics.model.ptr
     data = physics.data.ptr
     viewer = mujoco_viewer.MujocoViewer(model, data)
 
     # simulate and render
-    for _ in range(5000):
+    is_contact = False
+    pregrasp_step = 0
+    while(not is_contact):
         if viewer.is_alive:
-            physics.data.qpos[:30] = np.zeros(30)
+            # physics.data.qpos[:30] = np.zeros(30)
+            # physics.data.qvel[:30] = np.zeros(30)
+            for i in range(retarget_pose.shape[0]):
+                physics.data.qpos[:30] = retarget_pose[16]
             physics.data.qvel[:30] = np.zeros(30)
 
             physics.step()
+            is_contact = np.linalg.norm(physics.named.data.xpos[45] - ori_obs_pos) > 0.03
+            pregrasp_step += 1
             viewer.render()
         else:
             break
+
+    print(pregrasp_step)
     
     # close
     viewer.close()
