a0f000d8344041bef392fb1b694f3fb08c00460f
zerchen <zeruicasia@gmail.com>
2023-11-30 18:27:07 +0100

    update trajs and fix vis_pregrasp

--- vis_pregrasp.py
+++ vis_pregrasp.py
@@ -8,6 +8,7 @@ from fire import Fire
 from base import MjModel
 import numpy as np
 import pickle
+import random
 import mujoco_viewer
 import mujoco
 import os
@@ -69,106 +70,114 @@ def object_generator(path):
     return __XMLObj__
 
 
-def main(traj_name='ycb-002_master_chef_can-20200709-subject-01-20200709_141754'):
-    with open('trajectories/retarget_result_dexycb.pkl', 'rb') as f:
-        traj_robots = pickle.load(f)
+def main(traj_name=None):
+    if traj_name is None:
+        traj_names = os.listdir('trajectories/ycb')
+        traj_names = [name.split('.')[0] for name in traj_names]
+        random.shuffle(traj_names)
+    else:
+        traj_names = [traj_name]
 
-    index = '-'.join(traj_name.split('-')[2:-1]) + '/' + traj_name.split('-')[-1]
-    retarget_pose = np.array(traj_robots[index]['robot_joints'])
+    for traj_name in traj_names:
+        index = '-'.join(traj_name.split('-')[2:-1]) + '/' + traj_name.split('-')[-1]
 
-    object_category = traj_name.split('-')[0]
-    object_name = traj_name.split('-')[1]
+        object_category = traj_name.split('-')[0]
+        object_name = traj_name.split('-')[1]
 
-    traj_path = os.path.join(f'trajectories/{object_category}/{traj_name}.npz')
-    traj_file = np.load(traj_path, allow_pickle=True)
-    traj_file =  {k:v for k, v in traj_file.items()}
-    traj_file['s_0'] = traj_file['s_0'][()]
+        traj_path = os.path.join(f'trajectories/{object_category}/{traj_name}.npz')
+        traj_file = np.load(traj_path, allow_pickle=True)
+        traj_file =  {k:v for k, v in traj_file.items()}
+        traj_file['s_0'] = traj_file['s_0'][()]
 
-    init_object_translation = np.array(traj_file['s_0']['pregrasp']['object_translation'])
-    init_object_orientation = np.array(traj_file['s_0']['pregrasp']['object_orientation'])
+        init_object_translation = np.array(traj_file['s_0']['pregrasp']['object_translation'])
+        init_object_orientation = np.array(traj_file['s_0']['pregrasp']['object_orientation'])
+        retarget_pose = np.array(traj_file['s_0']['pregrasp']['robot_position'])
 
-    object_translation = np.array(traj_file['object_translation'])
-    object_orientation = np.array(traj_file['object_orientation'])
-    hand_joint = np.array(traj_file['hand_joint'])
-    pregrasp_joint = np.array(traj_file['s_0']['pregrasp']['position'])
+        object_translation = np.array(traj_file['object_translation'])
+        object_orientation = np.array(traj_file['object_orientation'])
+        hand_joint = np.array(traj_file['hand_joint'])
+        pregrasp_joint = np.array(traj_file['s_0']['pregrasp']['position'])
 
-    # object_translation[:, :2] -= init_object_translation[:2]
-    # hand_joint[:, :, :2] -= init_object_translation[:2]
-    # pregrasp_joint[:, :2] -= init_object_translation[:2]
-    # init_object_translation[:2] -= init_object_translation[:2]
+        env = TableEnv()
+        robot_model = get_robot('adroit')(limp=False)
+        robot_mesh_list = robot_model.mjcf_model.find_all('geom')
+        robot_geom_names = [geom.get_attributes()['name'] for geom in robot_mesh_list]
+        robot_geom_names = [f'adroit/{name}' for name in robot_geom_names if 'C' in name]
+        env.attach(robot_model)
 
-    # beta = np.random.uniform(low=0.0, high=2*np.pi)
-    # rot_matrix = np.array([[np.cos(beta), -np.sin(beta), 0], [np.sin(beta), np.cos(beta), 0], [0, 0, 1]])
-    # object_translation = (rot_matrix @ object_translation.transpose(1, 0)).transpose(1, 0)
-    # for idx in range(object_orientation.shape[0]):
-        # object_orientation[idx] = Quaternion(matrix=(rot_matrix @ Quaternion(object_orientation[idx]).rotation_matrix)).elements
-    # hand_joint = (rot_matrix[None] @ hand_joint.transpose(0, 2, 1)).transpose(0, 2, 1)
-    # pregrasp_joint = (rot_matrix @ pregrasp_joint.transpose(1, 0)).transpose(1, 0)
-    # init_object_translation = rot_matrix @ init_object_translation
-    # init_object_orientation = Quaternion(matrix=rot_matrix @ Quaternion(init_object_orientation).rotation_matrix).elements
+        object_model = object_generator(f"objects/{object_category}/{object_name}.xml")(pos=init_object_translation, quat=init_object_orientation)
+        object_mesh_list = object_model.mjcf_model.find_all('geom')
+        object_geom_names = [geom.get_attributes()['name'] for geom in object_mesh_list]
+        object_geom_names = [f'{object_name}/{name}' for name in object_geom_names if 'contact' in name]
 
-    # new_init_pos = np.random.uniform(low=-0.15, high=0.15, size=2)
-    # object_translation[:, :2] += new_init_pos 
-    # hand_joint[:, :, :2] += new_init_pos
-    # pregrasp_joint[:, :2] += new_init_pos
-    # init_object_translation[:2] += new_init_pos
+        object_model.mjcf_model.worldbody.add('body', name=f'hand_palm', pos=pregrasp_joint[0])
+        object_model.mjcf_model.worldbody.body[f'hand_palm'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_palm_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+        object_model.mjcf_model.worldbody.add('body', name=f'hand_thumb', pos=pregrasp_joint[4])
+        object_model.mjcf_model.worldbody.body[f'hand_thumb'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_thumb_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+        object_model.mjcf_model.worldbody.add('body', name=f'hand_index', pos=pregrasp_joint[8])
+        object_model.mjcf_model.worldbody.body[f'hand_index'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_index_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+        object_model.mjcf_model.worldbody.add('body', name=f'hand_middle', pos=pregrasp_joint[12])
+        object_model.mjcf_model.worldbody.body[f'hand_middle'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_middle_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+        object_model.mjcf_model.worldbody.add('body', name=f'hand_ring', pos=pregrasp_joint[16])
+        object_model.mjcf_model.worldbody.body[f'hand_ring'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_ring_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+        object_model.mjcf_model.worldbody.add('body', name=f'hand_little', pos=pregrasp_joint[20])
+        object_model.mjcf_model.worldbody.body[f'hand_little'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_little_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
 
-    env = TableEnv()
+        env.attach(object_model)
+        physics = physics_from_mjcf(env)
+        ori_obs_pos = physics.named.data.xpos[45].copy()
 
-    robot_model = get_robot('adroit')(limp=False)
-    robot_mesh_list = robot_model.mjcf_model.find_all('geom')
-    robot_geom_names = [geom.get_attributes()['name'] for geom in robot_mesh_list]
-    robot_geom_names = [f'adroit/{name}' for name in robot_geom_names if 'C' in name]
-    env.attach(robot_model)
+        model = physics.model.ptr
+        data = physics.data.ptr
 
-    object_model = object_generator(f"objects/{object_category}/{object_name}.xml")(pos=init_object_translation, quat=init_object_orientation)
-    object_mesh_list = object_model.mjcf_model.find_all('geom')
-    object_geom_names = [geom.get_attributes()['name'] for geom in object_mesh_list]
-    object_geom_names = [f'{object_name}/{name}' for name in object_geom_names if 'contact' in name]
+        is_contact = False
+        pregrasp_step = 9
+        for idx in range(retarget_pose.shape[0]):
+            if idx >= 9:
+                physics.reset()
+                physics.data.qpos[:30] = retarget_pose[idx]
+                physics.data.qvel[:30] = np.zeros(30)
 
-    object_model.mjcf_model.worldbody.add('body', name=f'hand_palm', pos=pregrasp_joint[0])
-    object_model.mjcf_model.worldbody.body[f'hand_palm'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_palm_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
-    object_model.mjcf_model.worldbody.add('body', name=f'hand_thumb', pos=pregrasp_joint[4])
-    object_model.mjcf_model.worldbody.body[f'hand_thumb'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_thumb_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
-    object_model.mjcf_model.worldbody.add('body', name=f'hand_index', pos=pregrasp_joint[8])
-    object_model.mjcf_model.worldbody.body[f'hand_index'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_index_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
-    object_model.mjcf_model.worldbody.add('body', name=f'hand_middle', pos=pregrasp_joint[12])
-    object_model.mjcf_model.worldbody.body[f'hand_middle'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_middle_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
-    object_model.mjcf_model.worldbody.add('body', name=f'hand_ring', pos=pregrasp_joint[16])
-    object_model.mjcf_model.worldbody.body[f'hand_ring'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_ring_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
-    object_model.mjcf_model.worldbody.add('body', name=f'hand_little', pos=pregrasp_joint[20])
-    object_model.mjcf_model.worldbody.body[f'hand_little'].add('geom', type='sphere', contype='0', conaffinity='0', mass='0', name=f'hand_little_visual', size="0.01", rgba=np.array([1, 0, 0, 1]))
+                physics.forward()
+                is_contact = check_contacts(physics, robot_geom_names, object_geom_names)
+                if is_contact:
+                    break
+                pregrasp_step += 1
 
-    env.attach(object_model)
-    physics = physics_from_mjcf(env)
-    ori_obs_pos = physics.named.data.xpos[45].copy()
+        pregrasp_step -= 1
 
-    model = physics.model.ptr
-    data = physics.data.ptr
-    viewer = mujoco_viewer.MujocoViewer(model, data)
+        object_translation[:, :2] -= init_object_translation[:2]
+        hand_joint[:, :, :2] -= init_object_translation[:2]
+        pregrasp_joint[:, :2] -= init_object_translation[:2]
+        retarget_pose[:, 0] += init_object_translation[0]
+        retarget_pose[:, 2] -= init_object_translation[1]
+        init_object_translation[:2] -= init_object_translation[:2]
 
-    # simulate and render
-    is_contact = False
-    pregrasp_step = 0
-    while(not is_contact):
-        if viewer.is_alive:
-            # physics.data.qpos[:30] = np.zeros(30)
-            # physics.data.qvel[:30] = np.zeros(30)
-            for i in range(retarget_pose.shape[0]):
-                physics.data.qpos[:30] = retarget_pose[16]
-            physics.data.qvel[:30] = np.zeros(30)
+        new_init_pos = np.random.uniform(low=-0.15, high=0.15, size=2)
+        object_translation[:, :2] += new_init_pos
+        hand_joint[:, :, :2] += new_init_pos
+        pregrasp_joint[:, :2] += new_init_pos
+        init_object_translation[:2] += new_init_pos
+        retarget_pose[:, 0] -= new_init_pos[0]
+        retarget_pose[:, 2] += new_init_pos[1]
 
-            physics.step()
-            is_contact = np.linalg.norm(physics.named.data.xpos[45] - ori_obs_pos) > 0.03
-            pregrasp_step += 1
-            viewer.render()
-        else:
-            break
+        physics.reset()
+        physics.model.body_pos[45] = init_object_translation
+        physics.model.body_pos[46:] = pregrasp_joint[[0, 4, 8, 12, 16, 20]]
+        viewer = mujoco_viewer.MujocoViewer(model, data)
+        # simulate and render
+        for _ in range(1000):
+            if viewer.is_alive:
+                physics.data.qpos[:30] = retarget_pose[pregrasp_step]
+                physics.data.qvel[:30] = np.zeros(30)
 
-    print(pregrasp_step)
-    
-    # close
-    viewer.close()
+                physics.step()
+                viewer.render()
+            else:
+                break
+
+        # close
+        viewer.close()
 
 if __name__ == '__main__':
     Fire(main)
