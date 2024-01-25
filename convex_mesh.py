7bd50167887202838c49e95af8dc5bd5e9f5d89b
zerchen <zeruicasia@gmail.com>
2024-01-25 10:04:02 +0100

    add hoi4d meshes

--- convex_mesh.py
+++ convex_mesh.py
@@ -19,6 +19,7 @@ def create_collision_mesh(obj_path, output_path):
 
 
 if __name__ == '__main__':
+    for dexycb
     classes = ['002_master_chef_can','003_cracker_box','004_sugar_box','005_tomato_soup_can','006_mustard_bottle','007_tuna_fish_can','008_pudding_box','009_gelatin_box','010_potted_meat_can','011_banana','019_pitcher_base','021_bleach_cleanser','024_bowl','025_mug','035_power_drill','036_wood_block','037_scissors','040_large_marker','051_large_clamp','052_extra_large_clamp','061_foam_brick']
     for obj_name in classes:
         object_path = f'/home/zerui/workspace/code/object_sim/ycb/{obj_name}'
@@ -26,23 +27,18 @@ if __name__ == '__main__':
         object_mesh = trimesh.load(os.path.join(object_path, 'textured.obj'), process=False)
         object_mesh = object_mesh.apply_translation(-object_mesh.centroid)
         object_mesh.export(os.path.join(object_path, 'textured.obj'))
-
         create_collision_mesh(os.path.join(object_path, 'textured.obj'), object_path)
 
     # for hoi4d
-    # classes = ['Bottle', 'Bowl', 'Kettle', 'Knife', 'Mug', 'ToyCar']
-    # hoi4d_model_path = '/home/zerui/workspace/code/dex_sim/datasets/hoi4d/HOI4D_CAD_Model_for_release/rigid'
-
+    # classes = ['Mug']
+    # hoi4d_model_path = '/home/zerui/workspace/code/dex_sim/datasets/hoi4d/data/HOI4D_CAD_Model_for_release/rigid'
     # for obj_class in classes:
         # for filename in os.listdir(os.path.join(hoi4d_model_path, obj_class)):
             # instance_id = filename.split('.')[0].zfill(4)
-
-            # visual_path = f'/home/zerui/workspace/code/dex_sim/hand_imitation/env/models/assets/hoi4d_{obj_class}/visual/{instance_id}'
-            # collision_path = f'/home/zerui/workspace/code/dex_sim/hand_imitation/env/models/assets/hoi4d_{obj_class}/collision/{instance_id}'
-            # os.makedirs(visual_path, exist_ok=True)
-            # os.makedirs(collision_path, exist_ok=True)
-
-            # visual_mesh = trimesh.load(os.path.join(hoi4d_model_path, obj_class, filename)).simplify_quadric_decimation(5000)
-            # visual_mesh.export(os.path.join(visual_path, 'model_transform_scaled.stl'))
-            # # create_collision_mesh(os.path.join(hoi4d_model_path, obj_class, filename), collision_path)
+            # object_path = f'/home/zerui/workspace/code/object_sim/hoi4d/{instance_id}_{obj_class}'
+            # os.makedirs(object_path, exist_ok=True)
+            # object_mesh = trimesh.load(os.path.join(hoi4d_model_path, obj_class, filename)).simplify_quadric_decimation(5000)
+            # object_mesh = object_mesh.apply_translation(-object_mesh.centroid)
+            # object_mesh.export(os.path.join(object_path, 'textured.obj'))
+            # create_collision_mesh(os.path.join(object_path, 'textured.obj'), object_path)
 
